4dbc1dc807dcb8989e6b4c1eaf5097893f44cc3e
FlavioAlmeida53 <flavio.almeida@gpnet.com.br>
2020-07-14 12:20:28 -0300

    Publicação de novo projeto

---
+++ delivery-eureka-service/delivery-eureka-service/src/test/java/br/com/jucesp/delivery/deliveryeurekaservice/DeliveryEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jucesp.delivery.deliveryeurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
